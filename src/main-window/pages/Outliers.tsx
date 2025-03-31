83ff6995dd5a1b68eb4efcece487bfd67568bec2
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2025-03-31 23:33:52 +0200

    outliers loading state

--- src/main-window/pages/Outliers.tsx
+++ src/main-window/pages/Outliers.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, Loader2 } from "lucide-react";
 import { _OutlierDetail, AlgorithmSettings } from "../../lib/models";
 import { TitleBar } from "../../components/TitleBar";
 import {
@@ -23,7 +23,7 @@ const OutlierCard = ({
       <CardHeader>
         <div className="flex items-center gap-4">
           <AlertCircle className="text-accent" size={24} />
-          <CardTitle className="text-lg text-text font-semibold select-text cursor-text">
+          <CardTitle className="cursor-text select-text text-lg font-semibold text-text">
             "{outlier.response.text}"
           </CardTitle>
         </div>
@@ -66,6 +66,7 @@ const OutlierCard = ({
 export default function Outliers() {
   const [outliers, setOutliers] = useState<_OutlierDetail[]>([]);
   const [outlierThreshold, setOutlierThreshold] = useState<number | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const [nearestNeighbors, setNearestNeighbors] = useState<number | null>(null);
   const [zScoreThreshold, setZScoreThreshold] = useState<number | null>(null);
@@ -73,24 +74,26 @@ export default function Outliers() {
   useEffect(() => {
     const unsubscribe = window.database.onReceiveCurrentOutliers(
       (outliersMessage) => {
+        setLoading(false);
         setOutliers(
-          outliersMessage.outliers.sort((a, b) => b.similarity - a.similarity)
+          outliersMessage.outliers.sort((a, b) => b.similarity - a.similarity),
         );
         setOutlierThreshold(outliersMessage.threshold);
-      }
+      },
     );
     return () => unsubscribe();
   }, []);
 
   useEffect(() => {
     window.database.requestCurrentOutliers();
+    setLoading(true);
   }, []);
 
   useEffect(() => {
     const unsubscribe = window.database.onReceiveCurrentRun(({ run }) => {
       if (run) {
         const settings = JSON.parse(
-          run.algorithm_settings
+          run.algorithm_settings,
         ) as AlgorithmSettings;
 
         setNearestNeighbors(settings.outlier_detection?.nearest_neighbors);
@@ -106,36 +109,53 @@ export default function Outliers() {
   }, []);
 
   return (
-    <div className="w-screen h-screen">
+    <div className="h-screen w-screen">
       <TitleBar index={5} />
       <div
         id="mainContent"
-        className="dark:dark flex flex-col bg-background px-32 pt-6 text-text gap-4 select-none"
+        className="dark:dark flex select-none flex-col gap-4 bg-background px-32 pt-6 text-text"
       >
         <div className="flex items-center justify-start">
           <h1 className="text-4xl">Response Outliers</h1>
         </div>
-        <div>
-          <p className="text-lg">
-            Displaying <span className="font-semibold">{outliers.length}</span>{" "}
-            outlier responses.
-          </p>
-          <p className="text-base">
-            These responses have a lower similarity to their{" "}
-            <span className="font-semibold">{nearestNeighbors}</span> nearest
-            neighbors compared to the threshold (Z-score:{" "}
-            <span className="font-semibold">{zScoreThreshold}</span>).
-          </p>
-        </div>
-        <div className="scrollbar pr-4 flex flex-grow flex-col gap-4 overflow-y-auto pt-0 pb-4">
-          {outliers.map((outlier, index) => (
-            <OutlierCard
-              key={index}
-              outlier={outlier}
-              threshold={outlierThreshold || 0}
-            />
-          ))}
-        </div>
+        {loading ? (
+          <div className="flex h-full w-full items-center justify-center gap-2">
+            <Loader2 className="size-8 animate-spin text-accent" />
+            <p className="text-2xl text-text">Loading...</p>
+          </div>
+        ) : (
+          <>
+            {outliers.length === 0 ? (
+              <div className="flex items-center gap-2">
+                <AlertCircle className="size-6 text-accent" />
+                <p className="text-lg">No outliers found.</p>
+              </div>
+            ) : (
+              <div>
+                <p className="text-lg">
+                  Displaying{" "}
+                  <span className="font-semibold">{outliers.length}</span>{" "}
+                  outlier responses.
+                </p>
+                <p className="text-base">
+                  These responses have a lower similarity to their{" "}
+                  <span className="font-semibold">{nearestNeighbors}</span>{" "}
+                  nearest neighbors compared to the threshold (Z-score:{" "}
+                  <span className="font-semibold">{zScoreThreshold}</span>).
+                </p>
+              </div>
+            )}
+            <div className="scrollbar flex flex-grow flex-col gap-4 overflow-y-auto pb-4 pr-4 pt-0">
+              {outliers.map((outlier, index) => (
+                <OutlierCard
+                  key={index}
+                  outlier={outlier}
+                  threshold={outlierThreshold || 0}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
